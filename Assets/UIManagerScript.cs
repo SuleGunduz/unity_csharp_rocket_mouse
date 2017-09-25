0bdcf1fe4e093f74551ba919da75615d8be68428
SuleGunduz <sule_gunduz16@hotmail.com>
2017-09-25 15:06:11 +0200

    rocket mouse UI&GUI

---
+++ Assets/UIManagerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIManagerScript : MonoBehaviour {
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+    public Animator contentPanel;
+    public Animator gearImage;
+
+    public void ToggleMenu()
+    {
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
+        gearImage.SetBool("isHidden", !isHidden);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("RocketMouse");
+
+    }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+
+
+
+}
+
